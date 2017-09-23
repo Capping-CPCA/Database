f4c160c30d61d056eb7881f119427d4010dbb1d2
dr-crow <James.Crowley1@marist.edu>
2017-09-23 13:23:03 -0400

    Added some more views from carson

--- views.sql
+++ views.sql
@@ -23,3 +23,61 @@ CREATE VIEW ClassAttendanceDetails AS
     AND ParticipantAttendanceDetails.siteName = TeacherAttendanceDetails.siteName
     INNER JOIN Sites
     ON ParticipantAttendanceDetails.siteName = Sites.siteName;
+
+	
+-- Facilitator Info --
+ SELECT facilitators.facilitatorid,
+    people.firstname,
+    people.lastname,
+    people.middleinit,
+    employees.email,
+    employees.primaryphone,
+    employees.permissionlevel,
+    facilitators.program,
+    facilitatorlangugage.lang,
+    facilitatorlangugage.level AS langlevel
+   FROM people,
+    facilitators,
+    employees,
+    facilitatorlangugage
+  WHERE people.peopleid = employees.employeeid AND employees.employeeid = facilitators.facilitatorid AND facilitators.facilitatorid = facilitatorlangugage.facilitatorid
+  ORDER BY facilitators.facilitatorid;
+  
+ -- Family Info	--
+ 
+CREATE OR REPLACE VIEW public."FamilyInfo" AS 
+ SELECT family.formid AS familyid,
+    people.peopleid,
+    people.firstname,
+    people.lastname,
+    people.middleinit,
+    familymembers.relationship,
+    familymembers.dateofbirth,
+    familymembers.race,
+    familymembers.sex
+   FROM people,
+    familymembers,
+    family
+  WHERE people.peopleid = familymembers.familymemberid AND familymembers.familymemberid = family.familymembersid
+  ORDER BY family.formid;
+
+-- Participant Status
+CREATE OR REPLACE VIEW public."ParticipantStatus" AS 
+ SELECT participants.participantid,
+    people.firstname,
+    people.lastname,
+    people.middleinit,
+    participants.dateofbirth,
+    participants.race,
+    participantclassattendance.topicname AS mostrecentclass,
+    participantclassattendance.date,
+    max(atttotal.totalclasses) AS totalclasses
+   FROM people,
+    participants,
+    participantclassattendance,
+    ( SELECT participantclassattendance_1.participantid,
+            row_number() OVER (ORDER BY participantclassattendance_1.participantid) AS totalclasses
+           FROM participantclassattendance participantclassattendance_1) atttotal
+  WHERE people.peopleid = participants.participantid AND participants.participantid = participantclassattendance.participantid
+  GROUP BY participants.participantid, people.firstname, people.lastname, people.middleinit, participants.dateofbirth, participants.race, participantclassattendance.topicname, participantclassattendance.date
+  ORDER BY participants.participantid;
