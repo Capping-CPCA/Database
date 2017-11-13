52202f9a6fe9fbab4895223810c34999bedb761f
jrands <John.Randis1@marist.edu>
2017-11-13 16:42:47 -0500

    added not null constraint to notes

--- TABLES.sql
+++ TABLES.sql
@@ -586,7 +586,7 @@ CREATE TABLE IF NOT EXISTS EmergencyContactDetail (
  */
 CREATE TABLE IF NOT EXISTS Notes (
     noteID        SERIAL                    NOT NULL UNIQUE,
-    content       TEXT,
+    content       TEXT,                     NOT NULL
     date          DATE DEFAULT CURRENT_DATE NOT NULL,
     participantID INT                       NOT NULL,
     employeeID    INT                       NOT NULL,
